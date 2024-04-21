42e8c0ba30e49fcfc833c918e0c8fed72b13fa5e
Artem <101066894+ArtemFilumenov@users.noreply.github.com>
2024-04-21 17:55:29 +0300

    add openModal, addCard, task

---
+++ src/data.js
@@ -0,0 +1,82 @@
+export const tasks = [
+    {
+        id: 1,
+        date: '30.01.2023',
+        topic: "Web Design",
+        title: "Название задачи",
+        status: "Без статуса",
+
+    },
+    {
+        id: 2,
+        date: '30.01.2023',
+        topic: "Research",
+        title: "Название задачи",
+        status: "Нужно сделать",
+
+    },
+    {
+        id: 3,
+        date: '1.10.2023',
+        topic: "Copywriting",
+        title: "Название задачи",
+        status: "В работе",
+
+    },
+    {
+        id: 4,
+        date: '30.01.2024',
+        topic: "Research",
+        title: "Название задачи",
+        status: "Тестирование",
+
+    },
+    {
+        id: 5,
+        date: '30.02.2024',
+        topic: "Copywriting",
+        title: "Название задачи",
+        status: "Готово",
+
+    },
+    // {
+    //     id: 6,
+    //     date: '30.1.2023',
+    //     topic: "Web Design",
+    //     title: "Название задачи",
+    //     status: "Без статуса",
+
+    // },
+    // {
+    //     id: 7,
+    //     date: '30.1.2023',
+    //     topic: "Web Design",
+    //     title: "Название задачи",
+    //     status: "Без статуса",
+
+    // },
+    // {
+    //     id: 8,
+    //     date: '30.1.2023',
+    //     topic: "Web Design",
+    //     title: "Название задачи",
+    //     status: "Без статуса",
+
+    // },
+    // {
+    //     id: 9,
+    //     date: '30.1.2023',
+    //     topic: "Web Design",
+    //     title: "Название задачи",
+    //     status: "Без статуса",
+
+    // },
+    // {
+    //     id: 10,
+    //     date: '30.1.2023',
+    //     topic: "Web Design",
+    //     title: "Название задачи",
+    //     status: "Без статуса",
+
+    // },
+]
